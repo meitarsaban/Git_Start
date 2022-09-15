39eafcddb6426c5094d29fb4b1d580d96512f097
meitarsaban <meitarsaban7890@gmail.com>
2022-09-15 11:58:19 +0300

    test

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+#1
+print(" Hello Enter your  name: ")
+
+#2
+print(" Hello Enter your adress: ")
+
+
+
+name = "Name"
+print(" Hello " , name ," How are you: " )
+print("your name")
